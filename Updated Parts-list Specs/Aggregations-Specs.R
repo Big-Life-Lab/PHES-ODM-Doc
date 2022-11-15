6a292adf1345459ba0c6583024b4a5e7626362a8
mathew-thomson <43016327+mathew-thomson@users.noreply.github.com>
2022-11-15 10:54:40 -0500

    Update Aggregation, add additional files to populate
    Finished aggregation specifications, added other files to populate later.

--- Updated Parts-list Specs/Aggregations-Specs.R
+++ Updated Parts-list Specs/Aggregations-Specs.R
@@ -1,4 +1,7 @@
-aggregatiions
+Aggregations
 
 
-[partID where partType == "aggregation"]: fe,f e
+**[partLabel where partType == "aggregation"]** ([partID where partType == "aggregation"]): [partDesc where partType == "aggregation"]. [partInstr where partType == "aggregation"; if = NA print = F]
+*"Part Type:"* [partType]
+*"Aggregation Scale:"* [aggScaleID]
+*"Aggregation Sets"* [in sets.csv list([setID]) where "partID" = partID]
