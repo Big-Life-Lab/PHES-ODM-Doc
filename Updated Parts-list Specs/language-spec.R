a17c1a7016de6c356dfdbec778b7b8a9f215b49a
mathew-thomson <43016327+mathew-thomson@users.noreply.github.com>
2022-11-15 13:38:52 -0500

    Update language spec
    wrote and pushed language spec

--- Updated Parts-list Specs/language-spec.R
+++ Updated Parts-list Specs/language-spec.R
@@ -1,2 +1,10 @@
-language
+languages
 
+[Native Name] (Language ID)	
+"Language Family:" [Language family]	
+"Language name:" [Language name]		
+"ISO639-1 code:" [ISO639-1]	
+"ISO639-2B code:" [ISO639-2B]	
+"ISO639-2T code:"	[ISO639-2T]
+"ISO639-3 code:" [ISO639-3]	
+"ISO639-6 code:" [ISO639-6]
