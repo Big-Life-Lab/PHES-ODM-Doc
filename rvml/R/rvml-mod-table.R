1e9c67b43485eb88292bd948d58348ea711a010c
Rostyslav <rostikisawesome@gmail.com>
2022-12-14 11:26:52 -0500

    [Refactor] Added stylistic changes

--- rvml/R/rvml-mod-table.R
+++ rvml/R/rvml-mod-table.R
@@ -26,18 +26,29 @@ for (single_category in cat_sets) {
   working_rows <- working_rows[grepl("^NA", rownames(working_rows))==F,]
   part_id <- part_id[grepl("^NA", rownames(part_id))==F,]
   
-  new_col[[part_id$partID]] <- working_rows$partID
+  new_col[[part_id$partID]] <- list(partID = working_rows$partID, partLabel = working_rows$partLabel)
 }
 
 new_table <- val_table
 new_table[[col_name]] <- ""
 for (single_partID in names(new_col)) {
   
-  sub_categories <- new_col[[single_partID]]
-  col_value <- ""
-  for (single_categorie in sub_categories) {
-    col_value <- paste0(col_value, "<a href=\"#",single_categorie,"\">",single_categorie,"</a></br>")
+  sub_categories <- new_col[[single_partID]][[1]]
+  col_value <- "Categorical inputs for this part </br><ul>"
+  
+  # Skip empty cat values for now
+  if(length(sub_categories)<1){
+    print(paste0(single_partID," is tagged with catSet but has no sub categories"))
+    next()
+    
   }
+  
+  for (single_categorie_index in 1:length(sub_categories)) {
+    cat_value_ID <- new_col[[single_partID]]$partID[[single_categorie_index]]
+    cat_label <- new_col[[single_partID]]$partLabel[[single_categorie_index]]
+    col_value <- paste0(col_value, "<li><a href=\"#",cat_value_ID,"\">","**",cat_label,"**", ":(",cat_value_ID,")","</a></li>")
+  }
+  col_value <- paste0(col_value,"</ul>")
   new_table[[col_name]][new_table$partID == single_partID] <- col_value
   
 }
