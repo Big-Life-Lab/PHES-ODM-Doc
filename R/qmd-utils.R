d97311a33ad5c81e0bd9aafe44af5dc238a838e4
rvyuha <rostikisawesome@gmail.com>
2023-02-07 14:53:37 -0500

    [Feature] Added Roxygen documentation to util functions

--- R/qmd-utils.R
+++ R/qmd-utils.R
@@ -1,4 +1,13 @@
-# Utility function to check for column existence in data_source
+#' Has Column for Table
+#' 
+#' Function to check for existence of column in data source. 
+#' Creates an appropriate warning and returns boolean based on its presence.
+#' 
+#' @param data_source data.frame which might contain the column.
+#' @param table_name the name of the table whose column is being checked.
+#' @param column_name the name of the column being checked.
+#' 
+#' @return boolean equal to existence of column_name column in data_source.
 has_column_for_table <-
   function(data_source, table_name, column_name) {
     column_is_valid <- TRUE
@@ -11,6 +20,17 @@ has_column_for_table <-
     return(column_is_valid)
   }
 
+#' Format Input
+#' 
+#' Formats input to make it compliant with glue,
+#' as well as issuing warning to the location of missing data.
+#' 
+#' @param string_to_check the string being checked for being empty.
+#' @param part_ID Id of the part in string to check, optional parameter used in construction of the warning.
+#' @param string_source_name Name of the table string_to_check comes from, optional parameter used in construction of the warning.
+#' @param optional_warning A warning overwrite string to bypass warning creation when partID or table name are missing.
+#' 
+#' @return string_to_check as is or modified into "" if it fails the check.
 format_input <-
   function(string_to_check,
            part_ID = NULL,
