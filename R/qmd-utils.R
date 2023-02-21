0dddc98626ac479e1d9de209adf38a15dca9f69b
rvyuha <rostikisawesome@gmail.com>
2023-02-21 08:44:30 -0500

    [Refactor] Moved utility out of sets.qmd Added additional warnings and checks for sets

--- R/qmd-utils.R
+++ R/qmd-utils.R
@@ -55,22 +55,22 @@ verify_string <-
     if (string_to_check == constants$dictionary_missing_value_replacement) {
       is_valid_string <- FALSE
       warning(glue::glue('{warning_text}
-                         
+
                          '))
     }
     return(is_valid_string)
   }
 
 #' Verify and append
-#' 
+#'
 #' A utility function used to simplify content insertion based on the validity of passed content.
-#' 
+#'
 #' @param existing_content string you wish to append to
 #' @param content_to_verify string that is checked for validity and is used to determine what content is added
 #' @param verify_warning string containing the warning to print in case content_to_verify fails verification
 #' @param is_valid_insert string that is appended to existing_content if content_to_verify is valid
 #' @param is_invalid_insert string that is appended to existing_content if content_to_verify is invalid
-#' 
+#'
 #' @return returns existing_content with either valid or invalid insert depending on the validity of content_to_verify
 verify_and_append_content <-
   function(existing_content,
@@ -79,11 +79,37 @@ verify_and_append_content <-
            is_valid_insert,
            is_invalid_insert) {
     return_content <- ""
-    if(verify_input(content_to_verify, verify_warning)){
+    if (verify_input(content_to_verify, verify_warning)) {
       return_content <- glue::glue('{existing_content}{is_valid_insert}')
-    }else{
-      return_content <- glue::glue('{existing_content}{is_invalid_insert}')
+    } else{
+      return_content <-
+        glue::glue('{existing_content}{is_invalid_insert}')
     }
     
     return(return_content)
   }
+
+
+#' Create html list
+#'
+#' Utility function for creating html list out of an R list and appending it to a string.
+#'
+#' @param string_to_append a string containing the string you wish to append to
+#' @param list_values a R list containing elements you wish to add to the string as html list
+#'
+#' @return string containing list_values as an html list
+create_html_list <- function(string_to_append, list_values, warning_text) {
+  string_to_append <- glue::glue('{string_to_append}
+                                 <ul>', .null = "NA")
+  for (list_element in list_values) {
+    if (verify_input(list_element, warning_text)) {
+      string_to_append <- glue::glue('{string_to_append}
+                                   <li> {list_element} </li>', .null = "NA")
+    } else{
+      next()
+    }
+  }
+  string_to_append <- glue::glue('{string_to_append}
+                                 </ul>', .null = "NA")
+  return(string_to_append)
+}
