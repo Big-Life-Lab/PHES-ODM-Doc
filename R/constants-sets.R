838c83679f72b4889f92b4f313c8387943a3dbf9
rvyuha <rostikisawesome@gmail.com>
2023-03-17 10:09:23 -0400

    [Refactor] Split up the constants

---
+++ R/constants-sets.R
@@ -0,0 +1,5 @@
+sets_sheet_column_names <- list()
+
+# Column names for sets sheet
+sets_sheet_column_names$part_set_type_column_name <- "setType"
+sets_sheet_column_names$part_set_ID_column_name <- "set"
