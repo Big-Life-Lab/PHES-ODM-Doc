085a67e781b596583a93433fe45b5e2c0b50c618
rvyuha <rostikisawesome@gmail.com>
2023-03-30 17:23:59 -0400

    [Test] Testing moving file reading back to qmd

--- R/version-utils.R
+++ R/version-utils.R
@@ -6,11 +6,9 @@ source("R/constants.R")
 #' @param file_names string or string vector containing file names with valid semantic versioning
 #'
 #' @return vector of strings containing latest version and latest version file name
-get_latest_version <- function() {
+get_latest_version <- function(file_names) {
   dictionary_version_pattern <- "ODM_dictionary_(.*?).xlsx"
-  file_names <-
-    list.files(file.path(getwd(), constants$dictionary_directory),
-               pattern = "^ODM.*xlsx$")
+  
   # Display warning for multiple dictionaries as only 1 should be stored on github
   if (length(file_names) > 1) {
     warning('Multiple dictionaries found only one dictionary should be stored.')
